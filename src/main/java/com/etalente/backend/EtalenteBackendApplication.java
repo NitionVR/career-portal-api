01919e66fcaa60852034a067adbf592d8cfd5af6
NitionVR64 <amosmaganyane@gmail.com>
2025-09-15 21:57:49 +0200

    feat: Initialize Spring Boot project and configure basic setup

---
+++ src/main/java/com/etalente/backend/EtalenteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.etalente.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtalenteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtalenteBackendApplication.class, args);
+	}
+
+}
