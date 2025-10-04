a434527166db3c42cd49b8a5ad8be23ece8a747b
NitionVR64 <amosmaganyane@gmail.com>
2025-10-04 17:59:51 +0200

    test(acceptance): Configure tests to use maildev for email testing

--- src/test/java/com/etalente/backend/acceptance/AcceptanceTest.java
+++ src/test/java/com/etalente/backend/acceptance/AcceptanceTest.java
@@ -16,7 +16,8 @@ import org.springframework.context.annotation.Import;
 @TestPropertySource(properties = {
         "spring.datasource.url=jdbc:tc:postgresql:15-alpine:///etalente",
         "spring.datasource.driver-class-name=org.testcontainers.jdbc.ContainerDatabaseDriver",
-        "application.security.jwt.secret-key=NDg0MDRkNjM1MTY2NTQ2YTU3NmU1YTcyMzQ3NTM3NzgyMTQxMjU0NDJhNDcyZDRiNjE1MDY0NTM2NzU2NmI1OQ=="
+        "application.security.jwt.secret-key=NDg0MDRkNjM1MTY2NTQ2YTU3NmU1YTcyMzQ3NTM3NzgyMTQxMjU0NDJhNDcyZDRiNjE1MDY0NTM2NzU2NmI1OQ==",
+        "cloud.aws.ses.from=test@example.com"
 })
 class AcceptanceTest {
 
